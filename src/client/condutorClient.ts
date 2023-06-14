402539cb4ae6e5454e44059147a4474e099d263f
Rafael Ch <rafael.cherman0@gmail.com>
2023-06-13 21:25:20 -0300

    criado condutorClient e seus metodos

---
+++ src/client/condutorClient.ts
@@ -0,0 +1,75 @@
+import axios, {AxiosInstance} from "axios";
+import { Condutor } from "@/model/condutor";
+
+//(await this.axiosClient.get<COndutor>(`/${id}`)).data
+
+export class CondutorClient {
+
+    private axiosClient: AxiosInstance;
+
+    constructor(){
+        this.axiosClient = axios.create(
+            {
+                baseURL: 'http://localhost:8080/api/condutor',
+                headers: {'Content-Type' : 'application/json'}
+            }
+        )
+    }
+
+
+    public async findById(id: number) : Promise<Condutor> {
+        try {
+            return (await this.axiosClient.get<Condutor>(`/${id}`)).data;
+        } 
+        catch (error:any) {
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async listAll() : Promise<Condutor[]> {
+        try {
+            return (await this.axiosClient.get<Condutor[]>(`/lista`)).data;
+        } 
+        catch (error:any) {
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async findAtivo() : Promise<Condutor[]> {
+        try {
+            return (await this.axiosClient.get<Condutor[]>(`/ativo`)).data;
+        } 
+        catch (error:any) {
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async cadastrar(condutor: Condutor) : Promise<string> {
+        try {
+            return (await this.axiosClient.post<string>('/', condutor)).data;
+        } 
+        catch (error:any) {
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async editar(id: number, condutor: Condutor) : Promise<string> {
+        try {
+            return (await this.axiosClient.post<string>(`/${id}`, condutor)).data;
+        } 
+        catch (error:any) {
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async deletar(id:number) : Promise<string> {
+        try {
+            return (await this.axiosClient.delete<string>(`/${id}`)).data;
+        } 
+        catch (error:any) {
+            return Promise.reject(error.response);
+        }
+    }
+
+
+}
