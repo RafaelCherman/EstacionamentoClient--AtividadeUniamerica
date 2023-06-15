b205e65b13b09c05bcef678df67f6f5d20a2c21a
Rafael Ch <rafael.cherman0@gmail.com>
2023-06-14 21:19:54 -0300

    testando routers

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/marca">Marcas</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style lang="scss">
+
+// Variable overrides first
+$primary: #900;
+$enable-shadows: true;
+$prefix: "mo-";
+
+// Then import Bootstrap
+@import "../node_modules/bootstrap/scss/bootstrap";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
